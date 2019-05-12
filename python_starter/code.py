b11cb7a2b65941dd46859a61cd61d7da280dcbd0
cgrinaldi <cgrinaldi@gmail.com>
2019-05-12 11:41:04 -0700

    Setup project skeleton

---
+++ python_starter/code.py
@@ -0,0 +1,8 @@
+def add(x, y):
+    """Add two numbers.
+
+    :params x: int, first number to add
+    :params y: int, second number to add
+    :returns: int, the sum of the two numbers
+    """
+    return x + y
---
+++ tests/test_basic.py
@@ -0,0 +1,13 @@
+from python_starter import code
+
+
+def test_will_fail():
+    assert False
+
+
+def test_will_pass():
+    assert True
+
+
+def test_add():
+    assert code.add(3, 5) == 8
